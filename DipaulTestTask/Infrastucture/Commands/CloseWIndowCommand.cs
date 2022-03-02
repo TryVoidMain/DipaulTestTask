42646f39fc79bfe0550fe66622071535e43f8201
Kosnstantin <tryvoidmain@gmail.com>
2022-03-02 22:31:25 +0300

    Реализовал первого потомка класса команды, для закрытия программы при переходе "Файл -> Выход". Привязал свойство Title MainWindowViewModel к свойству Title пердставления.

---
+++ DipaulTestTask/Infrastucture/Commands/CloseWIndowCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Linq;
+using DipaulTestTask.Infrastucture.Commands.Base;
+
+namespace DipaulTestTask.Infrastucture.Commands
+{
+    class CloseWIndowCommand : Command
+    {
+        protected override void Execute(object p)
+        {
+            var window = p as Window;
+            if (window is null)
+                window = Application.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(w => w.IsFocused);
+
+            if (window is null)
+                window = Application.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(w => w.IsActive);
+            
+            window?.Close();
+        }
+    }
+}
