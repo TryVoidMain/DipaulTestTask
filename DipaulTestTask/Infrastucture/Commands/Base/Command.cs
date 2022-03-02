fe2663e1a4af6112f0afea672d7ddb4c3fc8a505
Kosnstantin <tryvoidmain@gmail.com>
2022-03-02 22:29:02 +0300

    Создал базовый класс команды

---
+++ DipaulTestTask/Infrastucture/Commands/Base/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DipaulTestTask.Infrastucture.Commands.Base
+{
+    public abstract class Command : ICommand
+    {
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object p) => CanExecute(p);
+
+        void ICommand.Execute(object p) => Execute(p);
+
+        protected virtual bool CanExecute(object p) => true;
+
+        protected abstract void Execute(object p);
+    }
+}
