54183e55b864cb92d4795fa942d9364d35540f80
Kosnstantin <tryvoidmain@gmail.com>
2022-03-02 22:28:47 +0300

    Почистил базовый класс ViewModel Добавил класс ViewModel главного окна программы.

---
+++ DipaulTestTask/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DipaulTestTask.ViewModels.Base;
+
+namespace DipaulTestTask.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Главное окно программы";
+        public string Title 
+        { 
+            get => _Title; 
+            set => Set(ref _Title, value); 
+        }
+
+    }
+}
