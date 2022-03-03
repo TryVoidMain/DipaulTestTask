216fb63020a2c3c0325d3788bae7e56025deae23
Kosnstantin <tryvoidmain@gmail.com>
2022-03-04 02:06:53 +0300

    Добавил свойств и комманд в модель-представления, связал их с представлением.

--- DipaulTestTask/ViewModels/MainWindowViewModel.cs
+++ DipaulTestTask/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,12 @@
-﻿using DipaulTestTask.ViewModels.Base;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.IO;
+using System.Collections.Generic;
+
+using DipaulTestTask.ViewModels.Base;
+using DipaulTestTask.Models;
+using DipaulTestTask.Infrastucture.Commands;
 
 namespace DipaulTestTask.ViewModels
 {
@@ -10,6 +18,60 @@ namespace DipaulTestTask.ViewModels
             get => _Title; 
             set => Set(ref _Title, value); 
         }
+
+        private ObservableCollection<Company> _Companies;
+        public ObservableCollection<Company> Companies
+        {
+            get => _Companies;
+            set => Set(ref _Companies, value);
+        }
+
+        private ObservableCollection<Employee> _Employees;
+        public ObservableCollection<Employee> Employees
+        {
+            get => _Employees;
+            set => Set(ref _Employees, value);
+        }
+
+        private ICommand _LoadDataCommand;
+        public ICommand LoadDataCommand => _LoadDataCommand ??= new LambdaCommand(
+    OnLoadDataCommandExecuted);
+
+        private void OnLoadDataCommandExecuted(object p)
+        {
+            Companies = new ObservableCollection<Company>(TestData.TestData.Companies);
+            Employees = new ObservableCollection<Employee>();
+        }
+
+        private Company _SelectedCompany;
+        public Company SelectedCompany
+        {
+            get => _SelectedCompany;
+            set 
+            { 
+                Set(ref _SelectedCompany, value);
+                Employees.Clear();
+                foreach(Employee employee in _SelectedCompany.Employees)
+                {
+                    Employees.Add(employee);
+                }
+            }
+        }
+
+        private Employee _SelectedEmployee;
+        public Employee SelectedEmployee
+        {
+            get => _SelectedEmployee;
+            set => Set(ref _SelectedEmployee, value);
+        }
+
+        private void OnEmployeePositionSourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+            if (e.Property.Name == SelectedEmployee.Pos.ToString())
+                return;
+            SelectedEmployee.Pos = (Position)Enum.Parse(typeof(Position), e.Property.Name);
+        }
+
 
     }
 }
