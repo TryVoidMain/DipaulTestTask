9749e006d09452f03427ece468fe32b701df1ac8
Kosnstantin <tryvoidmain@gmail.com>
2022-03-06 12:31:20 +0300

    Добавил обобщённый интерфейс IStorage, описывает структуру хранилища

---
+++ DipaulTestTask/Interfaces/IStorage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using DipaulTestTask.Models;
+
+namespace DipaulTestTask.Interfaces
+{
+    public interface IStorage<T>
+    {
+        ICollection<T> Items { get; }
+
+        void Load();
+
+        void SaveChanges();
+    }
+
+    public interface ICompanyStorage : IStorage<Company> { }
+
+}
