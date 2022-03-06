6691d93a1d6cae2b2d34ab8f146517cba2a137f4
Kosnstantin <tryvoidmain@gmail.com>
2022-03-06 12:32:19 +0300

    Создания хранилища данных в файле Xml

---
+++ DipaulTestTask/Service/DataStorageInXmlFile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+using DipaulTestTask.Interfaces;
+using DipaulTestTask.Models;
+
+namespace DipaulTestTask.Service
+{
+    public class DataStorageInXmlFile : ICompanyStorage
+    {
+        public class DataStructure
+        {
+            public List<Company> Companies { get; set; } = new List<Company>();
+        }
+
+        private readonly string _FileName;
+
+        public DataStorageInXmlFile(string fileName) => _FileName = fileName;
+
+        private DataStructure Data { get; set; } = new DataStructure();
+
+        ICollection<Company> IStorage<Company>.Items => Data.Companies;
+
+        public void Load()
+        {
+            if(!File.Exists(_FileName))
+            {
+                Data = new DataStructure();
+                return;
+            }
+
+            using var file = File.OpenText(_FileName);
+            if(file.BaseStream.Length==0)
+            {
+                Data = new DataStructure();
+                return;
+            }
+
+            var serializer = new XmlSerializer(typeof(DataStructure));
+            Data = (DataStructure)serializer.Deserialize(file);
+        }
+
+        public void SaveChanges()
+        {
+            using var file = File.CreateText(_FileName);
+            var serializer = new XmlSerializer(typeof(DataStructure));
+            serializer.Serialize(file, Data);
+        }
+
+    }
+}
