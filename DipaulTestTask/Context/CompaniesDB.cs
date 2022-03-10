8689100d9a012ae8d6e3b89bb0b95d6b1764cd09
Kosnstantin <tryvoidmain@gmail.com>
2022-03-10 22:05:47 +0300

    Добавил контекст данных БД

---
+++ DipaulTestTask/Context/CompaniesDB.cs
@@ -0,0 +1,12 @@
+﻿using DipaulTestTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DipaulTestTask.Context
+{
+    public class CompaniesDB : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public CompaniesDB(DbContextOptions<CompaniesDB> opt):base(opt) { }
+    }
+}
