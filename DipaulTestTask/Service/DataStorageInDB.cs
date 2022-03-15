082a08d74b3247b5941fa05e30894ef8b7f1782d
Kosnstantin <tryvoidmain@gmail.com>
2022-03-15 22:33:25 +0300

    Добавил инициализацию DataBase

--- DipaulTestTask/Service/DataStorageInDB.cs
+++ DipaulTestTask/Service/DataStorageInDB.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions;
@@ -24,6 +24,77 @@ namespace DipaulTestTask.Service
             using (var db = new CompaniesDB(connection_options))
             {
                 db.Database.EnsureCreated();
+
+                if(!db.Companies.Any())
+                {
+                    var companies = new List<Company>
+                    {
+                        new Company
+                        {
+                            Id = 1,
+                            Name = "Диполь",
+                            Employees = new List<Employee>
+                            {
+                                new Employee
+                                {
+                                    Id = 1,
+                                    Name = "Vasya",
+                                    Pos = Position.Developer
+                                },
+                                new Employee
+                                {
+                                    Id = 2,
+                                    Name = "Petya",
+                                    Pos = Position.Manager
+                                },
+                                new Employee
+                                {
+                                    Id = 3,
+                                    Name = "Vasilisa",
+                                    Pos = Position.CEO
+                                }
+
+                            }
+                        },
+
+                        new Company
+                        {
+                            Id = 1,
+                            Name = "OneAss",
+                            Employees = new List<Employee>
+                            {
+                                new Employee
+                                {
+                                    Id = 1,
+                                    Name = "Petya",
+                                    Pos = Position.Developer
+                                },
+                                new Employee
+                                {
+                                    Id = 2,
+                                    Name = "Vasya",
+                                    Pos = Position.CEO
+                                },
+                                new Employee
+                                {
+                                    Id = 3,
+                                    Name = "Vasilisa",
+                                    Pos = Position.Manager
+                                }
+
+                            }
+                        }
+                    };
+
+                    foreach(Company company in companies)
+                        db.Companies.Add(company);
+
+                    db.SaveChanges();
+                }
+
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
             }
         }        
     }
